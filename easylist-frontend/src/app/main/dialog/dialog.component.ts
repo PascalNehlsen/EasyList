bcea419cb27828b092c47aec153b94d40e993e2c
Pascal Nehlsen <pascaldev92@gmail.com>
2024-10-28 14:39:59 +0100

    chore: added dialog and service

--- easylist-frontend/src/app/main/dialog/dialog.component.ts
+++ easylist-frontend/src/app/main/dialog/dialog.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
   standalone: true,
-  imports: [],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
-export class DialogComponent {}
+export class DialogComponent {
+  constructor(public dialogRef: MatDialogRef<DialogComponent>) {}
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
--- easylist-frontend/src/app/main/plan-purchase/plan-purchase.component.ts
+++ easylist-frontend/src/app/main/plan-purchase/plan-purchase.component.ts
@@ -1,26 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DialogComponent } from '../dialog/dialog.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ArrayService } from '../../services/array.service';
 
 @Component({
   selector: 'app-plan-purchase',
   standalone: true,
-  imports: [RouterModule, DialogComponent],
+  imports: [RouterModule, DialogComponent, MatDialogModule],
   templateUrl: './plan-purchase.component.html',
   styleUrl: './plan-purchase.component.scss',
 })
 export class PlanPurchaseComponent {
-  categories: string[] = [
-    './assets/images/image-categories/obst.png',
-    './assets/images/image-categories/brot.png',
-    './assets/images/image-categories/suessigkeiten.png',
-    './assets/images/image-categories/muesli.png',
-    './assets/images/image-categories/fleisch.png',
-    './assets/images/image-categories/gemuese.png',
-    './assets/images/image-categories/milchprodukte.png',
-    './assets/images/image-categories/getraenke.png',
-  ];
-
   getCategoryName(imagePath: string): string {
     const fileName = imagePath.split('/').pop()?.split('.').shift() || '';
 
@@ -31,5 +22,16 @@ export class PlanPurchaseComponent {
       .replace(/ss/g, 'ß');
 
     return withUmlauts.charAt(0).toUpperCase() + withUmlauts.slice(1);
+  }
+
+  categories: string[] = [];
+
+  constructor(public dialog: MatDialog, private dataService: ArrayService) {
+    this.categories = this.dataService.getCategories();
+  }
+
+  openDialog(index: number) {
+    const categoryName = this.getCategoryName(this.categories[index]);
+    this.dialog.open(DialogComponent);
   }
 }
