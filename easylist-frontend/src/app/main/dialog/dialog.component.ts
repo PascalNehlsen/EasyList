af4d18f3596da8bc244e328aabc022ba66f70987
Pascal Nehlsen <pascaldev92@gmail.com>
2024-11-06 14:15:47 +0100

    chore: shopping list function

--- easylist-frontend/src/app/main/dialog/dialog.component.ts
+++ easylist-frontend/src/app/main/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ArrayService } from '../../services/array.service';
 
 @Component({
   selector: 'app-dialog',
@@ -9,12 +10,25 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrl: './dialog.component.scss',
 })
 export class DialogComponent {
+  shoppingList: string[];
+
   constructor(
     public dialogRef: MatDialogRef<DialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: string[]
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: string[],
+    private arrayService: ArrayService
+  ) {
+    this.shoppingList = this.arrayService.shoppingList;
+  }
 
   closeDialog() {
     this.dialogRef.close();
+  }
+
+  moveToList($index: number) {
+    let i = $index;
+    console.log(this.data[i]);
+
+    this.shoppingList.push(this.data[i]);
+    console.log(this.shoppingList);
   }
 }
--- easylist-frontend/src/app/main/make-purchase/make-purchase.component.ts
+++ easylist-frontend/src/app/main/make-purchase/make-purchase.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ArrayService } from '../../services/array.service';
 
 @Component({
   selector: 'app-make-purchase',
@@ -8,4 +9,14 @@ import { RouterModule } from '@angular/router';
   templateUrl: './make-purchase.component.html',
   styleUrl: './make-purchase.component.scss',
 })
-export class MakePurchaseComponent {}
+export class MakePurchaseComponent implements OnInit {
+  ngOnInit() {
+    // Holt die shoppingList vom ArrayService
+    this.shoppingList = this.arrayService.getShoppingList();
+    console.log(this.shoppingList); // Zeigt die Liste in der Konsole
+  }
+
+  shoppingList: string[] = [];
+
+  constructor(private arrayService: ArrayService) {}
+}
